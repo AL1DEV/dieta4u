b9760b70ce14814e70fc504704c7b28ad7fe5894
dmitroavramenko <Cosonic>
2017-06-24 14:25:34 +0300

    test push

--- sites/all/modules/field_slideshow/field_slideshow.js
+++ sites/all/modules/field_slideshow/field_slideshow.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.field_slideshow = {
     attach: function(context) {
 
-      for (i in Drupal.settings.field_slideshow) {
+      for (var i in Drupal.settings.field_slideshow) {
         var settings = Drupal.settings.field_slideshow[i],
           slideshow = $('div.' + i),
           num_slides = slideshow.children().length,
@@ -39,10 +39,10 @@
             resizing: 0,
             fx: settings.fx,
             speed: settings.speed,
-            timeout: parseInt(settings.timeout),
+            timeout: parseInt(settings.timeout, 10),
             index: i,
             settings: settings
-          }
+          };
 
           if (settings.speed == "0" && settings.timeout == "0") options.fastOnEvent = true;
           if (settings.controls) {
@@ -51,7 +51,7 @@
           }
           if (settings.pause) options.pause = true;
 
-          if (settings.pager != '') {
+          if (settings.pager !== '') {
             if (settings.pager == 'number' || settings.pager == 'image') options.pager = "#" + i + "-pager";
             if ((settings.pager == 'image' || settings.pager == 'carousel') && num_slides > 1) {
               options.pagerAnchorBuilder = function(idx, slide) {
@@ -59,9 +59,9 @@
               };
               if (settings.pager == 'carousel') {
                 var carouselops = {
-                  visible: parseInt(settings.carousel_visible),
-                  scroll: parseInt(settings.carousel_scroll),
-                  animation: parseInt(settings.carousel_speed),
+                  visible: parseInt(settings.carousel_visible, 10),
+                  scroll: parseInt(settings.carousel_scroll, 10),
+                  animation: parseInt(settings.carousel_speed, 10),
                   vertical: settings.carousel_vertical,
                   initCallback: function(carousel) {
                     $(".jcarousel-prev").addClass('carousel-prev');
@@ -85,7 +85,7 @@
                   carouselops.buttonNextHTML = null;
                   carouselops.buttonPrevHTML = null;
                 }
-                if (parseInt(settings.carousel_circular)) carouselops.wrap = 'circular';
+                if (parseInt(settings.carousel_circular, 10)) carouselops.wrap = 'circular';
 
                 $("#" + i + "-carousel").jcarousel(carouselops);
                 // the pager is the direct item's parent element
@@ -112,11 +112,11 @@
             // If we are using the carousel make it follow the activeSlide
             // This will not work correctly with circular carousel until the version 0.3 of jcarousel
             // is released so we disble this until then
-            if (options.settings.pager == 'carousel' && parseInt(options.settings.carousel_follow) && parseInt(options.settings.carousel_circular) == 0) {
+            if (options.settings.pager === 'carousel' && parseInt(options.settings.carousel_follow, 10) && parseInt(options.settings.carousel_circular, 10) === 0) {
               var carousel = $("#" + options.index + "-carousel").data("jcarousel");
               carousel.scroll(nextIndex, true);
             }
-          }
+          };
 
           if (num_slides > 1) {
 
@@ -125,18 +125,17 @@
               //on hover
               slideshow.cycle(options).cycle("pause").hover(function() {
                 $(this).cycle('resume');
-              },function(){
+              }, function() {
                 $(this).cycle('pause');
               });
-            }
-            else {
+            } else {
               // Cycle!
               slideshow.cycle(options);
             }
 
             // After the numeric pager has been built by Cycle, add some classes for theming
             if (settings.pager == 'number') {
-              $('.field-slideshow-pager a').each(function(){
+              $('.field-slideshow-pager a').each(function() {
                 $this = $(this);
                 $this.addClass('slide-' + $this.html());
               });
@@ -184,19 +183,18 @@
             rebuild_max_height($images.parents('.field-slideshow'));
           });
         });
-      }
-      else {
-        $(window).load(function(){
-          $('.field-slideshow').each(function(){
-            rebuild_max_height($(this))
-          })
+      } else {
+        $(window).load(function() {
+          $('.field-slideshow').each(function() {
+            rebuild_max_height($(this));
+          });
         });
 
       }
-      $(window).resize(function(){
-        $('.field-slideshow').each(function(){
-          rebuild_max_height($(this))
-        })
+      $(window).resize(function() {
+        $('.field-slideshow').each(function() {
+          rebuild_max_height($(this));
+        });
       });
 
     }
